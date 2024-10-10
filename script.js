3e7c82ff636bd23d4016deef855e123bbf7bac42
Hasna Hena Mow <hasna.hena@welldev.io>
2024-10-10 15:51:28 +0600

    Completed update todo (edit and toggle complete)

--- script.js
+++ script.js
@@ -10,10 +10,54 @@ addTodoBtn.addEventListener("click", addTodo);
  */
 function addTodo() {
   const newTodo = document.createElement("li");
+
   newTodo.innerHTML = todoInput.value;
-  const editIcon = document.createElement("span");
+  newTodo.style.cursor = "pointer";
   newTodo.style.listStyleType = "circle";
+
+  // Add edit icon for making the todo editable later
+  const editIcon = document.createElement("button");
+  editIcon.innerHTML = "✏️";
+  editIcon.style.cursor = "pointer";
+  editIcon.style.marginLeft = "10px";
+
+  newTodo.appendChild(editIcon);
   taskList.append(newTodo);
 
   todoInput.value = "";
+}
+
+/**
+ * Update todo using Event Delegation to
+ * capture click and perform edit or complete todo action
+ */
+taskList.addEventListener("click", handleTodoClick);
+
+function handleTodoClick(event) {
+  const clickedElement = event.target;
+
+  // Handle marking todo as completed or not
+  if (clickedElement.tagName === "LI" && !clickedElement.isContentEditable) {
+    if (clickedElement.style.textDecoration === "") {
+      clickedElement.style.textDecoration = "line-through";
+      clickedElement.style.listStyleType = "disc";
+    } else {
+      clickedElement.style.textDecoration = "";
+      clickedElement.style.listStyleType = "circle";
+    }
+  }
+
+  // Handle editing the todo item when the edit button is clicked
+  if (clickedElement.tagName === "BUTTON") {
+    const todoItem = clickedElement.parentElement;
+
+    if (todoItem.isContentEditable) {
+      todoItem.contentEditable = "false";
+      clickedElement.innerHTML = "✏️";
+    } else {
+      todoItem.contentEditable = "true";
+      clickedElement.innerHTML = "✔️";
+      todoItem.focus();
+    }
+  }
 }
