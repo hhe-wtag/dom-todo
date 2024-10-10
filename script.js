428a594b394982ce0b2ba6f0a3c57dcf3f28a13c
Hasna Hena Mow <hasna.hena@welldev.io>
2024-10-10 15:17:51 +0600

    Completed adding todo

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const todoInput = document.getElementById("todo-input");
+const addTodoBtn = document.getElementById("add-todo-btn");
+const taskListContainer = document.getElementById("task-list-container");
+const taskList = document.getElementById("task-list");
 
+addTodoBtn.addEventListener("click", addTodo);
+
+/**
+ * Adding todo, clearing input after adding
+ */
+function addTodo() {
+  const newTodo = document.createElement("li");
+  newTodo.innerHTML = todoInput.value;
+  const editIcon = document.createElement("span");
+  newTodo.style.listStyleType = "circle";
+  taskList.append(newTodo);
+
+  todoInput.value = "";
+}
