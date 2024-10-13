e89488f448ed4b8dcb3172ca24afb9f5c7ff313e
Hasna Hena Mow <hasna.hena@welldev.io>
2024-10-13 11:03:16 +0600

    Refactor add todo based on review

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-const todoInput = document.getElementById("todo-input");
-const addTodoBtn = document.getElementById("add-todo-btn");
-const taskListContainer = document.getElementById("task-list-container");
-const taskList = document.getElementById("task-list");
+const $todoInput = document.getElementById("todo-input");
+const $addTodoBtn = document.getElementById("add-todo-btn");
+const $todoList = document.getElementById("todo-list");
 
-addTodoBtn.addEventListener("click", addTodo);
+$addTodoBtn.addEventListener("click", handleAddTodo);
 
-function addTodo() {
-  const newTodo = document.createElement("li");
-  newTodo.innerHTML = todoInput.value;
-  const editIcon = document.createElement("span");
-  newTodo.style.listStyleType = "circle";
-  taskList.append(newTodo);
+let todos = [];
 
-  todoInput.value = "";
+function handleAddTodo() {
+  const newTodo = {
+    id: Date.now(),
+    title: $todoInput.value,
+    createdAt: Date.now().toLocaleString(),
+  };
+  if (newTodo.title.trim() === "") return;
+  todos.push(newTodo);
+  $todoInput.value = "";
+  renderTodos();
+}
+
+function renderTodos() {
+  $todoList.innerHTML = "";
+  todos.forEach((todo) => {
+    $todoList.append(createTodo(todo));
+  });
+}
+
+function createTodo(todo) {
+  const $newTodo = document.createElement("li");
+  $newTodo.style.listStyleType = "none";
+  $newTodo.innerHTML = `<span>${todo.title}</span>`;
+
+  $newTodo.querySelector("span").addEventListener("click", () => {
+    $newTodo.classList.toggle("completed");
+  });
+
+  return $newTodo;
 }
